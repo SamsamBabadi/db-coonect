e82bde549e2d708b2bc4cd7f882ad3a54d5a524f
SamsamBabadi <samsambabadi@gmail.com>
2021-10-30 16:49:42 +0330

    change server ip to server name

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('books', function () {
     return JsonResource::collection(Book::get())->additional([
-        'server' => $_SERVER['SERVER_ADDR']
+        'server' => env('SERVER_NAME', 'server has no name yet!')
     ]);
 });
