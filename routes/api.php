a77f56ae787b1093a8577831e1bca231f9dfa090
SamsamBabadi <samsambabadi@gmail.com>
2021-10-30 15:06:48 +0330

    print server IP

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('books', function () {
-    return JsonResource::collection(Book::get());
+    return JsonResource::collection(Book::get())->additional([
+        'server' => $_SERVER['SERVER_ADDR']
+    ]);
 });
